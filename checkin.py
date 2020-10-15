b0032098ef03062e2fa48f2a2508ac43e537d66b
henrycoding <841636454@qq.com>
2020-10-15 11:57:37 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -17,7 +17,7 @@ def start():
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'content-type': 'application/json;charset=UTF-8'}, data='{"token":"glados_network"}')
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
    # print(res)
 
